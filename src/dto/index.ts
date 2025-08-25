e8c8da1f6385dac4c3acfbe00d99133b390f3c89
JesusEstebanBlancoSandoval <78365605+JesusEstebanBlancoSandoval@users.noreply.github.com>
2025-08-25 11:40:33 -0500

    index

---
+++ src/dto/index.ts
@@ -0,0 +1,8 @@
+/*
+para que es esto?: index.ts es un archivo que reexporta todos los dtos 
+para facilitar su importacion en otras partes de la aplicacion
+*
+*/
+export {CreateUserDTO}from './create-user.dto'
+export {loginUserDto}from './login-user.dto'
+export {updateUserDto}from './update-user.dto'
